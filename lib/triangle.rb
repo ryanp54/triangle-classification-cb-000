aa3b71098054e858a0f5af03d19121c46b893834
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-10 05:05:41 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,13 +5,8 @@ class Triangle
   end
 
   def kind
-    puts sides[0] <= 0
     if sides[0] <= 0 || sides[0] + sides[1] <= sides[2]
-      begin
-        raise TriangleError
-      rescue TriangleError => error
-          puts error.message
-      end
+      raise TriangleError
     elsif sides[0] == sides[2]
       return :equilateral
     elsif sides[0] == sides[1] || sides[1] == sides[2]
