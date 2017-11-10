2c41101fdf47c4453c963e954a7da316da63990c
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-10 04:45:26 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+  attr_accessor :sides
+  def initialize(a, b, c)
+      sides = [a, b, c].sort
+  end
+
+  def kind
+    if size[0] <= 0 || sides[0] + sides [1] <= sides[3]
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+          puts error
+      end
+    elsif sides[0] == sides[2]
+      return :equilateral
+    elsif sides[0] == sides[1] || sides[1] == sides[2]
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
+
 end
